56c223b55d3e94b5eb2c0ad2c690dc8606fd5974
Nat Williams <nat.williams@gmail.com>
2014-01-31 08:02:15 -0600

    specs for .run

--- spec/record_reader_spec.rb
+++ spec/record_reader_spec.rb
@@ -1,47 +1,54 @@
 require 'spec_helper'
 
-describe 'guess_separator' do
-  it "can error out" do
-    expect{ guess_separator('hey there') }.to raise_error
-  end
-
-  context "a reasonable csv" do
-    let (:data) {
-      'asdf, qwerty, foo\, bar, baz, qux'
-    }
-
-    it 'sees the commas' do
-      expect(guess_separator(data)).to eq ','
+describe RecordReader do
+  describe '.run' do
+    it "exits if you don't supply any filenames" do
+      expect{ RecordReader.run([]) }.to raise_error(SystemExit)
     end
-  end
 
-  context "a reasonable psv" do
-    let (:data) {
-      'asdf | qwerty | foo \| bar | baz | qux'
-    }
-
-    it "returns a pipe" do
-      expect(guess_separator(data)).to eq '|'
+    it "reads some records" do
+      RecordReader.stub(:parse_files).with(['foo', 'bar']).and_return([['a', 'b', 'c', 'd', '1/2/3']])
+      RecordReader.stub(:display) { |x| x }
+      RecordReader.run(['foo', 'bar', '-o', '1'])
     end
+
   end
 
-  context "a reasonable space separated value file" do
-    let (:data) {
-      'asdf qwerty foo\ bar baz qux'
-    }
-
-    it "returns a space" do
-      expect(guess_separator(data)).to eq ' '
+  describe '.guess_separator' do
+    it "can error out" do
+      expect{ RecordReader.guess_separator('hey there') }.to raise_error
     end
-  end
 
-  context "a tricky csv" do
-    let (:data) {
-      'a | a, b | b, c | c, d | d, e'
-    }
+    context "a reasonable csv" do
+      let (:data) { 'asdf, qwerty, foo\, bar, baz, qux' }
 
-    it "should recognize a csv" do
-      expect(guess_separator(data)).to eq ','
+      it 'sees the commas' do
+        expect(RecordReader.guess_separator(data)).to eq ','
+      end
+    end
+
+    context "a reasonable psv" do
+      let (:data) { 'asdf | qwerty | foo \| bar | baz | qux' }
+
+      it "returns a pipe" do
+        expect(RecordReader.guess_separator(data)).to eq '|'
+      end
+    end
+
+    context "a reasonable space separated value file" do
+      let (:data) { 'asdf qwerty foo\ bar baz qux' }
+
+      it "returns a space" do
+        expect(RecordReader.guess_separator(data)).to eq ' '
+      end
+    end
+
+    context "a tricky csv" do
+      let (:data) { 'a | a, b | b, c | c, d | d, e' }
+
+      it "should recognize a csv" do
+        expect(RecordReader.guess_separator(data)).to eq ','
+      end
     end
   end
 end
